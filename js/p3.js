6d2199a040a495a1ed47aa166e173429c47df722
Jacqueline Wilson <jwilson@fas.harvard.edu>
2013-12-02 17:42:14 -0500

    added swatches 12-2

--- js/p3.js
+++ js/p3.js
@@ -2,8 +2,13 @@ $( document ).ready(function() {
 	
 	var quiltCanvas = document.getElementById('quilt');
 	var quiltContext = quiltCanvas.getContext('2d');
-	
-	var rectColors = new Array('red','green','blue', 'purple', 'yellow');
+	var swatches = new Array();
+	var chosenSwatch = "not chosen";
+	var kineticSwatches = {};
+    var loadedImages = 0;
+
+		
+	// matches default of color picker:
 	var currentColor = "#333399";
 	
 	//Not used in final version but there if I need it
@@ -12,16 +17,52 @@ $( document ).ready(function() {
 		layer.draw();
 	}//end writeMessage
 
-	//Change square on block to colorpicker selection
-	function changeColor(shape) {
-		if (shape.colorIndex < rectColors.length-1){
-			shape.colorIndex=shape.colorIndex+1;
-		} else {
-			shape.colorIndex = 0;
-		}
-		shape.setFill(rectColors[shape.colorIndex]);
-		layer.draw();
-	}//end changeColor
+
+	// load swatches
+	// permission granted from David Butler, Amy Butler LTD http://www.amybutlerdesign.com
+	var dir = "images/amybutler/";
+	var fileextension = ".jpg";
+	var swatchNum = 0;
+
+	$.ajax({
+	    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+	    url: dir,
+	    success: function (data) {
+	        //List all png file names in the page
+	        //Code modified from http://stackoverflow.com/questions/18480550/how-to-load-all-the-images-from-one-of-my-folder-into-my-web-page-using-jquery
+	        $(data).find("a:contains(" + fileextension + ")").each(function () {
+	            var filename = this.href.replace(window.location.host, "").replace("http:///", "");
+	            $("#swatches").append("<img id='swatch" + swatchNum + "' src=" + dir + filename + " width=50 height=50 />");
+	            swatches[swatchNum]=dir+filename;
+	            console.log(dir+filename);
+	            swatchNum++;
+	        });//end data-find
+	    }//end success-data
+	});//end ajax
+	
+	//had to 'slow' this down so put it in a function, called when grid is created
+	function loadSwatches() {
+		console.log("swatch length is " + swatches.length);
+		for (var swatch=0; swatch<swatches.length; swatch++) {
+			swatchID = "swatch" + swatch;
+			kineticSwatches[swatchID] = new Image();
+			kineticSwatches[swatchID].onload = function() {};
+			kineticSwatches[swatchID].src = swatches[swatch];
+		}//end for
+	}//end function
+
+
+	//The way to bind event to dynamically created element it seems:
+	$(document).on( "click", "img", function(){
+		$('img').each(function() {
+			this.width=50;
+			this.height=50;
+		});		
+		chosenSwatch = this.id;
+		this.width=60;
+		this.height=60;
+	} );
+
 
 	//Creates quilt block grid based on user input of 9 or 16
 	function createQuiltBlock(size) {
@@ -42,10 +83,9 @@ $( document ).ready(function() {
 		var rectangles = new Array();
 		
 		var rect=0;
-	
+		
 		for (var row=0; row<blockSize; row+=100) {
 			for (var column=0; column<blockSize; column +=100){
-				console.log("rectangle ",rect, row, column);
 				rectangles[rect] = new Kinetic.Rect({
 					x: row,
 					y: column,
@@ -53,9 +93,10 @@ $( document ).ready(function() {
 					height: 100,
 					fill: "#E6E6B8",
 					stroke: 'gray',
-					strokeWidth: 1
+					strokeWidth: 1,
+					fillPatternImage: kineticSwatches.swatch4
 				});//end rectangle creation
-			rectangles[rect].colorIndex = 0;
+			rectangles[rect].colorIndex = 0; //added my own attribute
 			rect++;
 			} //end inner for
 		}//end outer for	  
@@ -72,15 +113,21 @@ $( document ).ready(function() {
 
 
 		// Events
-
 		for (var rect=0; rect<size; rect++) { 
 			rectangles[rect].on('tap', function() {
 				this.setFill(currentColor);
 				layer.draw();
 			});//end rectangle event
 			rectangles[rect].on('click', function() {
-				this.setFill(currentColor);
-				layer.draw();
+				if (chosenSwatch=="not chosen"){
+					this.setFill(currentColor);
+					layer.draw();
+				} else {
+					console.log(chosenSwatch);
+					this.setFill(null);//necessary to reset
+					this.setFillPatternImage(kineticSwatches[chosenSwatch]);
+					layer.draw();
+				}
 			});//end rectangle event
 		}//end for  
 	      
@@ -88,7 +135,7 @@ $( document ).ready(function() {
 		for (var rect=0; rect<size; rect++) {     
 			layer.add(rectangles[rect]);
 		}//end for
-	
+
 		//not used in final version but there if I need it
 		layer.add(text);
 	  
@@ -99,42 +146,27 @@ $( document ).ready(function() {
 	
 	$('#grid9').click(function(e) {
 		e.preventDefault();
+		loadSwatches();
 		createQuiltBlock(9);	  
 	});//end grid9 click
 	
 	$('#grid16').click(function(e) {
 		e.preventDefault();
+		loadSwatches();
 		createQuiltBlock(16);
 	});//end grid16 click
 	
 	$('#color1').colorPicker( { onColorChange : function(id, newValue) { 
+		chosenSwatch = "not chosen";//switches to color
 		currentColor = newValue;
 	}
 	});//end color picker event
-  
-  
-	// This is the only way I could find to access the Kinetic Stage's canvas since the
-	// provided layer.getCanvas() method doesn't seem to work in this revision of
-	// KineticJS
+ 
+    //The only canvas element on the screen is the Kinetic canvas
 	var quiltBlock = document.getElementsByTagName('canvas')[0];
-
-	// FOLLOWING CODE I'M KEEPING AROUND FOR REFERENCE
-	// WOULD LIKE TO CHANGE COLOR PICKER to FABRIC SWATCH PICKER
-	//  var imageObj1 = new Image();
-	//  imageObj1.src = "images/aqua_gingham.png"
-	//  imageObj1.onload = function() {
-	//    context1.drawImage(imageObj1, 0, 0);
-	//  };
-	
-	// var imageObj2 = new Image();
-	//  imageObj2.src = "images/feathers.png"
-	//  imageObj2.onload = function() {
-	//    context1.drawImage(imageObj2, 10, 10);
-	// context2.drawImage(imageObj2, 10, 10);
-	//  };
   
+	//Quilt-it event
 	$("#quiltIt").click(function(e) {
-		console.log('quilt button clicked');
 		var quiltBlock = document.getElementsByTagName('canvas')[0];
 		e.preventDefault();
 
